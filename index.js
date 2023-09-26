dfc7f003e8527af3c4114799c89c6afedeb0c761
David Stinnette <dastinnette@gmail.com>
2023-09-26 11:55:25 -0400

    Append individual dog details to middle column

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const baseURL = "http://localhost:3000/dogs"
+const loadURL = "http://localhost:3000/dogs/1"
 
 const gallery = document.querySelector('.dog-gallery')
+const displayDog = document.querySelector('.dog-details')
+
+fetch(loadURL)
+    .then(response => response.json())
+    .then(dog => {
+        renderSingleDog(dog)
+    })
 
 fetch(baseURL)
     .then(response => response.json())
@@ -16,4 +24,11 @@ function renderDog(dogObj) {
     img.alt = dogObj.name
     gallery.append(img)
     console.log(dogObj)
+}
+
+function renderSingleDog(dogObj) {
+    const img = document.createElement('img')
+    img.src = dogObj.image
+    img.alt = dogObj.name
+    displayDog.append(img)
 }
